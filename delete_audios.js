81e8bedd58cce04a47ee164dbec3966684e9d548
szy13 <41126478+szy13@users.noreply.github.com>
2020-08-19 13:43:32 +0300

    Update delete_audios.js

--- delete_audios.js
+++ delete_audios.js
@@ -17,19 +17,19 @@ scrollDown(function() {
     var audios = document.querySelectorAll(".audio_row");
     console.log(`Total songs: ${audios.length}`);
 
-    function del(audio_id) {
-        console.log(`delete ${audio_id.title}`);
+    function del(audio_obj) {
+        console.log(`delete ${audio_obj.title}`);
         ajax.post("al_audio.php", {
             act: "delete_audio",
-            oid: audio_id.ownerId,
-            aid: audio_id.id,
-            hash: audio_id.deleteHash,
+            oid: audio_obj.ownerId,
+            aid: audio_obj.id,
+            hash: audio_obj.deleteHash,
             restore: 1
         });
     }
 
     audios.forEach(function(el) {
-        let audio_id = AudioUtils.asObject(AudioUtils.getAudioFromEl(el));
-        del(audio_id);
+        let audio_obj = AudioUtils.asObject(AudioUtils.getAudioFromEl(el));
+        del(audio_obj);
     });
 });
